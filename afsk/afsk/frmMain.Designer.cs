d155ec13bd22f4938600be0d76ec946b9767b6ec
kazuhiro yokota <yokota.kazuhiro.cc@hotmail.co.jp>
2016-11-07 15:52:01 +0900

    Addtion of the WAV file output function

--- afsk/afsk/frmMain.Designer.cs
+++ afsk/afsk/frmMain.Designer.cs
@@ -32,23 +32,24 @@
             this.txtMessage = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnSettings = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnPlay
             // 
             this.btnPlay.Location = new System.Drawing.Point(125, 139);
-            this.btnPlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(2);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.Size = new System.Drawing.Size(78, 27);
             this.btnPlay.TabIndex = 0;
-            this.btnPlay.Text = "送信";
+            this.btnPlay.Text = "聞く";
             this.btnPlay.UseVisualStyleBackColor = true;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // txtMessage
             // 
             this.txtMessage.Location = new System.Drawing.Point(11, 22);
-            this.txtMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(2);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
             this.txtMessage.Size = new System.Drawing.Size(192, 102);
@@ -68,7 +69,7 @@
             // btnSettings
             // 
             this.btnSettings.Location = new System.Drawing.Point(11, 139);
-            this.btnSettings.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(2);
             this.btnSettings.Name = "btnSettings";
             this.btnSettings.Size = new System.Drawing.Size(78, 27);
             this.btnSettings.TabIndex = 4;
@@ -76,17 +77,28 @@
             this.btnSettings.UseVisualStyleBackColor = true;
             this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
             // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(11, 176);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(191, 24);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "WAVファイルの作成...";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(212, 175);
+            this.ClientSize = new System.Drawing.Size(212, 213);
+            this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.btnPlay);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
@@ -103,6 +115,7 @@
         private System.Windows.Forms.TextBox txtMessage;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
--- afsk/afsk/frmMain.cs
+++ afsk/afsk/frmMain.cs
@@ -19,19 +19,64 @@ namespace afsk
             InitializeComponent();
         }
 
-        private void btnPlay_Click(object sender, EventArgs e)
+        private void btnSettings_Click(object sender, EventArgs e)
         {
-            Stream stm = make_wave();
-            if (stm != null)
+            frmSettings dlg = new frmSettings();
+
+            dlg.preFlags = nPreFlags;
+            dlg.postFlags = nPostFlags;
+            dlg.destination = destination;
+            dlg.source = source;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
             {
-                SoundPlayer player = new SoundPlayer(stm);
-                player.Play();
+                nPreFlags = dlg.preFlags;
+                nPostFlags = dlg.postFlags;
+                destination = dlg.destination;
+                source = dlg.source;
             }
         }
 
-        private Stream make_wave()
+        private void btnPlay_Click(object sender, EventArgs e)
         {
-            string text = txtMessage.Text;
+            ax25wave ax25 = make_wave(txtMessage.Text);
+            if (ax25 != null)
+            {
+                waveFile wc = new waveFile(1, ax25wave.SAMPLE);
+                MemoryStream stm = new MemoryStream();
+                wc.save(stm, ax25.wave_data);
+                stm.Position = 0;
+                SoundPlayer player = new SoundPlayer(stm);
+                player.Play();
+                stm.Close();
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            ax25wave ax25 = make_wave(txtMessage.Text);
+            if (ax25 != null)
+            {
+                SaveFileDialog fd = new SaveFileDialog();
+
+                fd.FileName = "新しいファイル.wav";
+                fd.InitialDirectory = Directory.GetCurrentDirectory();
+                fd.Filter = "WAVファイル(*.wav)|*.wav|すべてのファイル(*.*)|*.*";
+                fd.FilterIndex = 1;
+                fd.Title = "保存先のファイルを選択してください";
+
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    waveFile wc = new waveFile(1, ax25wave.SAMPLE);
+                    FileStream ofs = new FileStream(fd.FileName, FileMode.Create, FileAccess.Write);
+                    wc.save(ofs, ax25.wave_data);
+                    ofs.Close();
+                }        
+            }
+        }
+
+        private ax25wave make_wave(string text)
+        {
             if (text.Length <= 0) return null;
 
             ax25wave ax25 = new ax25wave();     /* Waveデータの生成 */
@@ -49,11 +94,7 @@ namespace afsk
             for (int i = 0; i < nPostFlags; i++) ax25.send_flag_nrzi();
             ax25.end_sending();
 
-            waveFile wc = new waveFile(1, ax25wave.SAMPLE);
-            MemoryStream stm = new MemoryStream();
-            wc.save(stm, ax25.wave_data);
-            stm.Position = 0;
-            return stm;
+            return ax25;
         }
 
         private byte[] make_address(string csign, byte ssid)
@@ -75,24 +116,6 @@ namespace afsk
             }
             obj[6] = ssid;
             return obj;
-        }
-
-        private void btnSettings_Click(object sender, EventArgs e)
-        {
-            frmSettings dlg = new frmSettings();
-
-            dlg.preFlags = nPreFlags;
-            dlg.postFlags = nPostFlags;
-            dlg.destination = destination;
-            dlg.source = source;
-
-            if (dlg.ShowDialog() == DialogResult.OK)
-            {
-                nPreFlags = dlg.preFlags;
-                nPostFlags = dlg.postFlags;
-                destination = dlg.destination;
-                source =  dlg.source;
-            }
         }
     }
 }
